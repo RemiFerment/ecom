e5b9c7aaec04fb76aa58d2d2dc8ea71f16591a40
Rémi FERMENT <ferment.remi.pro@gmail.com>
2026-02-26 17:25:10 +0100

    Stabalize connection pdo

--- index.php
+++ index.php
@@ -4,5 +4,13 @@ require_once("scripts/connexion.php");
 
 include("template/head.phtml");
 include("template/header.phtml");
-include("template/index.phtml");
+
+// Initialisation of products
+
+$data = SelectConnectionPDO("SELECT * FROM products");
+if ($data !== null) {
+    include("template/index.phtml");
+} else {
+    echo '<h2>Une erreur inattendue s\'est produite.</h2>';
+}
 include("template/footer.phtml");
--- scripts/connexion.php
+++ scripts/connexion.php
@@ -1,9 +1,28 @@
 <?php
-$pdo = new PDO("mysql:host=127.0.0.1;dbname=ecom", "root", "root");
-$sql = "SELECT * FROM products";
 
-$select = $pdo->prepare($sql);
+function SelectConnectionPDO(string $sql): ?array
+{
+    $pdo = ConnectionPDO();
 
-$select->execute();
+    if ($pdo !== null) {
+        $select = $pdo->prepare($sql);
 
-$data = $select->fetchAll(PDO::FETCH_ASSOC);
+        if ($select->execute()) {
+            return $select->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+    return null;
+}
+
+function ConnectionPDO(string $dsn = "", string $user = "", string $password = ""): ?PDO
+{
+    $dsn = "mysql:host=127.0.0.1;dbname=ecom";
+    $user = "root";
+    $password = "root";
+    try {
+        return new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo "Une erreur est survenue : $e";
+    }
+    return null;
+}
